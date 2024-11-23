d7bae3b2a4dbf64113307a0e0eebb31432df3227
Nomozov <kamoliddinnomozov2606@gmail.com>
2024-11-22 17:41:15 -1200

    Gitignore Added

---
+++ 8-dars/Moduls/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_dars.Moduls
+{
+    internal class Car
+    {
+        public Guid CarId { get; set; }
+        public string CarType { get; set; }
+        public string CarName { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+
+    }
+}
---
+++ 8-dars/Services/CarServices.cs
@@ -0,0 +1,14 @@
+﻿using _8_dars.Moduls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_dars.Services
+{
+    internal class CarServices
+    {
+        private List<Car> cars;
+    }
+}
