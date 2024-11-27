e5290b511273aec7c10aa6d13d74fc9520cf91a1
Nomozov <kamoliddinnomozov2606@gmail.com>
2024-11-27 06:02:51 -1200

    NewCRUD

---
+++ 8-dars/10-dars CRUD/Services/BookServise.cs
@@ -0,0 +1,84 @@
+﻿using _10_dars_CRUD.Models;
+
+namespace _10_dars_CRUD.Services
+{
+
+    internal class BookServise
+    {
+        private List<Book> books;
+        public BookServise()
+        {
+            books = new List<Book>();
+        }
+        public Book AddBook(Book newbook)
+        {
+            newbook.Id = Guid.NewGuid();
+            books.Add(newbook);
+
+            return newbook;
+        }
+        public Book GetById(Guid id)
+        {
+            foreach (var bookDb in books)
+            {
+                if (bookDb.Id == id)
+                {
+                    return bookDb;
+                }
+
+            }
+            return null;
+        }
+        public bool DeletedBook(Guid id)
+        {
+            var bookDb = GetById(id);
+            if (bookDb == null)
+            {
+                return false;
+            }
+            books.Remove(bookDb);
+            return true;
+        }
+        public bool UpdateBook(Book newBook)
+        {
+            var bookDb = GetById(newBook.Id);
+            if (bookDb == null)
+            {
+                return false;
+            }
+            var index = books.IndexOf(bookDb);
+            books[index] = newBook;
+            return true;
+        }
+        public List<Book> GetAllBook()
+        {
+            return books;
+        }
+        public Book GetExpensiveBook()
+        {
+            var expensiveBook = new Book();
+           
+            foreach (var bookDb in books)
+            {
+                if (expensiveBook.Price < bookDb.Price)
+                {
+                    expensiveBook = bookDb;
+                }
+            }
+            return expensiveBook;
+        }
+        public Book GetMostPagedBook()
+        {
+            var mostpageBook = new Book();
+            foreach(var bookDb in books)
+            {
+                if(mostpageBook.PageNumber < bookDb.PageNumber)
+                {
+                    mostpageBook = bookDb;
+                }
+            }
+            return mostpageBook;
+        }
+        
+    }
+}
